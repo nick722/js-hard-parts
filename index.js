5cf974ebcd5cfcd0c783cce86672de51e1c8c213
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-09-02 18:32:51 +0300

    WIP Callbacks and Higher-Order Functions. Ex3 Intersection

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-//Extension 2
-var nums = [4, 1, 3];
-var add = function(a, b) { return a + b; }
-function reduce(array, callback, initialValue) {
-    var output = initialValue;
-    for (let i = 0; i < array.length; i++) {
-        output = callback(output, array[i]);
-    }
+//Extension 3
+
+// Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs.
+// BONUS: Use reduce!
+
+
+function intersection(arrays) {
+    let output = arguments;
     return output;
 }
 
-console.log(reduce(nums, add, 0));
+console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
+// should log: [5, 15]
+
+
+// function reduce(array, callback, initialValue) {
+//     var output = initialValue;
+//     for (let i = 0; i < array.length; i++) {
+//         output = callback(output, array[i]);
+//     }
+//     return output;
+// }
