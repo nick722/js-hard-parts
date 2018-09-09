094d85f65c399f6d996830003024ef9eea1a5dd6
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-09-09 13:58:36 +0300

    Async. Challenge 2.3 Stop "Internal Hello!" messages with clearInterval

--- index.js
+++ index.js
@@ -2,11 +2,4 @@ let intervalID = setInterval(function () {
     console.log('Interval Hello!');
 }, 2000);
 
-setTimeout(clearAllIntervals, 10000);
-
-// ...your code above
-function clearAllIntervals() {
-    for (let i = 0; i < 10000; i++) {
-        clearInterval(intervalID);
-    }
-}
+clearInterval(intervalID);
