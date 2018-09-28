e0ac3f09cb300855a3896d4571c7af98d916552b
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-09-28 17:20:51 +0300

    Challenge 1 print the string using setTimeout

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 frontendmasters.com - Will Sentance JavaScript The Hard Parts 01.22.2018 7h9m
 
-Callbacks and Higher-Order Functions
-http://csbin.io/callbacks
+Promises
+http://csbin.io/promises
+
+Challenge 1
+Let's start by reviewing asynchronous functions!
+Using setTimeout, print the string 'Hello!' after 1000ms.Challenge 1
+Let's start by reviewing asynchronous functions!
+Using setTimeout, print the string 'Hello!' after 1000ms.
--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-//Extension 3
+// Challenge 1
 
-// Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs.
-// BONUS: Use reduce!
-
-
-function intersection(arrays) {
-    let output = arguments;
-    return output;
+function sayHello() {
+    setTimeout(function () {
+        console.log('Hello!');
+    }, 1000);
 }
 
-console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
-// should log: [5, 15]
-
-
-// function reduce(array, callback, initialValue) {
-//     var output = initialValue;
-//     for (let i = 0; i < array.length; i++) {
-//         output = callback(output, array[i]);
-//     }
-//     return output;
-// }
+// Uncomment the line below when ready
+sayHello(); // should log "Hello" after 1000ms
